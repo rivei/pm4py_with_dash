16b1d7ba5e94710f62eb7277d72537a1084d9d65
Wei Lin <weilin0304@gmail.com>
2019-12-02 03:54:35 +0800

    new style

--- python/dash_app.py
+++ python/dash_app.py
@@ -6,19 +6,20 @@ Created on Sun Nov 24 22:49:04 2019
 """
 
 # -*- coding: utf-8 -*-
-import base64
-import io
+#import base64
+#import io
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 import pandas as pd
-import pathlib
+#import pathlib
 
 from dash.dependencies import Input, Output, State
 from scipy import stats
+import pm4pyPlus as ppp
 
-import process_csv as pscsv
+#import process_csv as pscsv
 
 group_colors = {"control": "light blue", "reference": "red"}
 
@@ -27,16 +28,45 @@ app = dash.Dash(
 )
 server = app.server
 
-PATH = pathlib.Path(__file__).parent
-DATA_PATH = PATH.joinpath("data").resolve()
-default_csv_data = pd.read_csv(DATA_PATH.joinpath("running-example.csv"))
+#PATH = pathlib.Path(__file__).parent
+#DATA_PATH = PATH.joinpath("data").resolve()
+#default_csv_data = pd.read_csv(DATA_PATH.joinpath("running-example.csv"))
 #default_study_data = pd.read_csv(DATA_PATH.joinpath("study.csv"))
 
-# App Layout
+#from pm4py.objects.log.importer.xes import factory as xes_importer
+#default_log = xes_importer.apply(str(DATA_PATH.joinpath("receipt.xes")))
+
+testdf = ppp.trace_df[ppp.trace_df['id']==0]
+#
+#ttfig = go.Figure(data=[
+#    go.Bar(x=testdf['actseq'], y=testdf['id'],orientation='h')
+#])
+## Change the bar mode
+#ttfig.update_layout(barmode='stack')
+
+import plotly.express as px
+ttfig = px.bar(ppp.trace_df, x="actseq", y="id", color='actid', orientation='h',
+             #hover_data=["tip", "size"],
+             height=1000,
+             title='Top Trace')
+#tips = px.data.tips()
+#ttfig = px.bar(tips, x="total_bill", y="sex", color='day', orientation='h',
+#             hover_data=["tip", "size"],
+#             height=400,
+#             title='Restaurant bills')
+
+
+#time
+dtfig = px.scatter(ppp.sort_df, x="sT", y="sid", color="actid")
+                 #size='petal_length', hover_data=['petal_width'])
+
+mtxfig
+
+
 app.layout = html.Div(
     children=[
         # Error Message
-        html.Div(id="error-message"),
+#        html.Div(id="error-message"),
         # Top Banner
         html.Div(
             className="study-browser-banner row",
@@ -51,293 +81,125 @@ app.layout = html.Div(
                 html.H2(className="h2-title-mobile", children="PM4PY Demo"),
             ],
         ),
+                
+        # settings and key elements
+        html.Div(
+            className="row",
+            children=[
+                html.Div(
+                    className="user-control two columns",
+                    children=[
+                        html.H6("Event Log File"),
+                        dcc.Upload(
+                            id="upload-data",
+                            className="upload",
+                            children=html.Div(
+                                children=[
+                                    html.P("Drag and Drop or "),
+                                    html.A("Select Files"),
+                                ]
+                            ),
+                            accept=".csv,.xes"
+                        ),
+                    ],
+                ),
+                html.Div(
+                    id="info-container",
+                    className="row container-display",            
+                    children=[
+                        html.Div(
+                            [#html.H6(id="eventText"), 
+                                    html.H6(ppp.n_events),
+                                    html.P("No. of Events")],
+                            id="events",
+                            className="mini_container",
+                        ),
+                        html.Div(
+                            [#html.H6(id="caseText"), 
+                                    html.H6(ppp.n_cases),
+                                    html.P("Cases")],
+                            id="cases",
+                            className="mini_container",
+                        ),
+                        html.Div(
+                            [#html.H6(id="traceText"), 
+                                    html.H6(ppp.n_traces),
+                                    html.P("Traces")],
+                            id="traces",
+                            className="mini_container",
+                        ),
+                        html.Div(
+                            [#html.H6(id="activityText"),
+                                    html.H6(ppp.n_activities),
+                                    html.P("Activities")],
+                            id="acitivities",
+                            className="mini_container",
+                        ),
+                        html.Div(
+                            [#html.H6(id="resourceText"), 
+                                    html.P("Resources")],
+                            id="resources",
+                            className="mini_container",
+                        ),
+                    ],
+                ),
+            ],
+        ),
         # Body of the App
         html.Div(
-            className="row app-body",
+            id="content_container",
+            className="row tabs",
             children=[
-                # User Controls
-                html.Div(
-                    className="four columns card",
-                    children=[
-                        html.Div(
-                            className="bg-white user-control",
+                dcc.Tabs(
+                    id="content_tabs",
+                    value="ttrace",
+                    children= [
+                        dcc.Tab(
+                            label="Map",
+                            value="promap",
                             children=[
-                                html.Div(
-                                    className="padding-top-bot",
-                                    children=[
-                                        html.H6("Graphics dropdown"),
-#                                        dcc.Dropdown(id="study-dropdown"),
-                                    ],
-                                ),
-                                html.Div(
-                                    className="padding-top-bot",
-                                    children=[
-                                        html.H6("Choose the type of plot"),
-                                        dcc.RadioItems(
-                                            id="chart-type",
-                                            options=[
-                                                {
-                                                    "label": "Box Plot", 
-                                                    "value": "box",
-                                                },
-                                                {
-                                                    "label": "Violin Plot",
-                                                    "value": "violin",
-                                                },
-                                            ],
-                                            value="violin",  #default value
-                                            labelStyle={
-                                                "display": "inline-block",
-                                                "padding": "12px 12px 12px 0px",
-                                            },
-                                        ),
-                                    ],
-                                ),
-                                html.Div(
-                                    className="padding-top-bot",
-                                    children=[
-                                        html.H6("Event Log File"),
-                                        dcc.Upload(
-                                            id="upload-data",
-                                            className="upload",
-                                            children=html.Div(
-                                                children=[
-                                                    html.P("Drag and Drop or "),
-                                                    html.A("Select Files"),
-                                                ]
-                                            ),
-                                            accept=".csv,.xes"
-                                        ),
-                                    ],
-                                ),
+                                html.H3("Map"),
                             ],
-                        )
-                    ],
-                ),
-                # Graph
-                html.Div(
-                    className="eight columns card-left",
-                    children=[
-                        html.Div(
-                            className="bg-white",
+                        ),
+                        dcc.Tab(
+                            label="Top-Trace",
+                            value="ttrace",
                             children=[
-                                html.H5("Process Map"),
-#                                dcc.Graph(id="plot"),
+                                html.H3("top trace"),
+                                dcc.Graph(figure = ttfig),
                             ],
-                        )
-                    ],
+                        ),
+                        dcc.Tab(
+                            label="Sequence",
+                            value="seq",
+                            children=[
+                                html.H3("Sequence"),
+                            ],
+                        ),
+                        dcc.Tab(
+                            label="Time",
+                            value="tim",
+                            children=[
+                                html.H3("Time"),
+                                dcc.Graph(figure=dtfig),
+                            ],
+                        ),
+                        dcc.Tab(
+                            label="Resources",
+                            value="res",
+                            children=[
+                                html.H3("Resources"),
+                            ],
+                        ),
+                    ]
                 ),
-                dcc.Store(id="error", storage_type="memory"),
             ],
         ),
     ]
 )
 
-# Callback to generate error message
-# Also sets the data to be used
-# If there is an error use default data else use uploaded data
-@app.callback(
-    [
-        Output("error", "data"),
-        Output("error-message", "children"),
-#        Output("study-dropdown", "options"),
-#        Output("study-dropdown", "value"),
-    ],
-    [Input("upload-data", "contents")],
-)
-def update_error(contents):
 
-    error_status = False
-    error_message = None
-#    study_data = default_study_data
-
-    # Check if there is uploaded content
-    if contents:
-        content_type, content_string = contents.split(",")
-        decoded = base64.b64decode(content_string)
-
-        # Try reading uploaded file
-#        try:
-#            study_data = pd.read_csv(io.StringIO(decoded.decode("utf-8")))
-#
-#            missing_columns = {
-#                "group_id",
-#                "group_type",
-#                "reading_value",
-#                "study_id",
-#            }.difference(study_data.columns)
-#
-#            if missing_columns:
-#                error_message = html.Div(
-#                    className="alert",
-#                    children=["Missing columns: " + str(missing_columns)],
-#                )
-#                error_status = True
-#                study_data = default_study_data
-#
-#        # Data is invalid
-#        except Exception as e:
-#            error_message = html.Div(
-#                className="alert",
-#                children=["That doesn't seem to be a valid csv file!"],
-#            )
-#            error_status = True
-#            study_data = default_study_data
-#
-#    # Update Dropdown
-#    options = []
-#    if "test_article" in study_data.columns:
-#        test_articles = study_data.test_article.unique()
-#        for test_article in test_articles:
-#            for study in study_data.study_id[
-#                study_data.test_article == test_article
-#            ].unique():
-#                options.append(
-#                    {"label": f"{test_article} (study: {study})", "value": study}
-#                )
-#    else:
-#        for study in study_data.study_id.unique():
-#            options.append({"label": study, "value": study})
-#
-#    options.sort(key=lambda item: item["label"])
-#    value = options[0]["value"] if options else None
-#
-#    return error_status, error_message, options, value
-    return error_status, error_message
-
-# Callback to generate study data
-#@app.callback(
-#    Output("plot", "figure"),
-#    [Input("chart-type", "value"), Input("study-dropdown", "value")],
-#    [State("upload-data", "contents"), State("error", "data")],
-#)
-#def update_output(chart_type, study, contents, error):
-#    if study is None:
-#        return {}
-#
-#    if error or not contents:
-#        study_data = default_study_data
-#    else:
-#        content_type, content_string = contents.split(",")
-#        decoded = base64.b64decode(content_string)
-#        study_data = pd.read_csv(io.StringIO(decoded.decode("utf-8")))
-#
-#    study_data["reading_value"] = pd.to_numeric(
-#        study_data["reading_value"], errors="coerce"
-#    )
-#
-#    study_data = study_data[study_data.study_id == study]
-#    vehicle_readings = study_data["reading_value"][
-#        study_data["group_type"] == "control"
-#    ]
-#    data_range = study_data["reading_value"].max() - study_data["reading_value"].min()
-#
-#    test_stats = {}
-#    box_data = []
-#    violin_data = []
-#    for i, group_id in enumerate(study_data.group_id.unique()):
-#        try:
-#            group_name = study_data["group_name"][
-#                study_data.group_id == group_id
-#            ].values[0]
-#        except KeyError:
-#            group_name = group_id
-#
-#        group_type = study_data["group_type"][study_data.group_id == group_id].values[0]
-#        y_data = study_data["reading_value"][study_data.group_id == group_id]
-#
-#        try:
-#            subject_ids = study_data["subject_id"][study_data.group_id == group_id]
-#        except KeyError:
-#            subject_ids = None
-#
-#        t, p = stats.ttest_ind(vehicle_readings, y_data)
-#        test_stats[group_id] = {
-#            "t": t,
-#            "p": p,
-#            "pf": "p={:0.3f}".format(p) if p >= 0.001 else "p<0.001",
-#            "astrix": "***"
-#            if p <= 0.001
-#            else "**"
-#            if p <= 0.01
-#            else "*"
-#            if p <= 0.05
-#            else "",
-#            "max_y": y_data.max(),
-#            "index": i,
-#        }
-#
-#        box_data.append(
-#            go.Box(
-#                y=y_data,
-#                name=group_name,
-#                text=subject_ids,
-#                hoveron="points",
-#                boxmean=True,
-#                showlegend=False,
-#                boxpoints="all",
-#                pointpos=0,
-#                line={"color": group_colors.get(group_type, "green")},
-#            )
-#        )
-#
-#        violin_data.append(
-#            go.Violin(
-#                y=y_data,
-#                name=group_name,
-#                text=subject_ids,
-#                hoveron="points",
-#                meanline={"visible": True},
-#                showlegend=False,
-#                points="all",
-#                pointpos=0,
-#                line={"color": group_colors.get(group_type, "green")},
-#            )
-#        )
-#
-#    chart_data = {"box": box_data, "violin": violin_data}
-#
-#    reading_name = (
-#        study_data["reading_name"].unique()[0]
-#        if "reading_name" in study_data.columns
-#        else None
-#    )
-#
-#    if not vehicle_readings.empty:
-#        ref_groups = set(
-#            study_data.group_id[study_data.group_type == "reference"].unique()
-#        )
-#        control_groups = set(
-#            study_data.group_id[study_data.group_type == "control"].unique()
-#        )
-#        all_groups = set(study_data.group_id.unique())
-#        groups_to_annotate = all_groups - ref_groups - control_groups
-#        annotations = [
-#            dict(
-#                x=test_stats.get(group_id, {"index": "None"})["index"],
-#                y=test_stats[group_id]["max_y"]
-#                + data_range / (4 if chart_type == "violin" else 10),
-#                text="{}<br>{}".format(
-#                    test_stats[group_id]["astrix"], test_stats[group_id]["pf"]
-#                ),
-#                showarrow=False,
-#            )
-#            for group_id in groups_to_annotate
-#        ]
-#    else:
-#        annotations = None
-#
-#    figure = go.Figure(
-#        data=chart_data[chart_type],
-#        layout=go.Layout(
-#            margin=go.layout.Margin(t=0, r=50, b=50, l=50),
-#            yaxis=dict(title=dict(text=reading_name)),
-#            annotations=annotations,
-#        ),
-#    )
-#
-#    return figure
-
+    
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=True) 
---
+++ python/pm4pyPlus.py
@@ -0,0 +1,180 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  1 22:17:20 2019
+
+@author: Wei
+"""
+
+import pathlib
+import pandas as pd
+import numpy as np
+#import pytz
+from datetime import datetime, tzinfo,timedelta
+
+from pm4py.objects.log.importer.xes import factory as xes_importer
+from pm4py.objects.log.util import interval_lifecycle
+from pm4py.algo.filtering.log.attributes import attributes_filter as log_attributes_filter
+
+PATH = pathlib.Path(__file__).parent
+DATA_PATH = PATH.joinpath("data").resolve()
+log = xes_importer.apply(str(DATA_PATH.joinpath("receipt.xes")))
+# =============================================================================
+#enriched_log = interval_lifecycle.assign_lead_cycle_time(log)
+#endf['ts_utc'] = endf['time:timestamp'].apply(lambda i: i.tz_convert('UTC'))
+# enriched_log does not help much
+# 0	0.0	0.0	0.0	0.0	0.0	1.0	task-42933	Confirmation of receipt	Group 1	Resource21	2011-10-11 13:45:40.276000+02:00	2011-10-11 13:45:40.276000+02:00
+# 1	16800.0	0.0	16800.0	16800.0	0.0	0.0	task-42935	T02 Check confirmation of receipt	Group 4	Resource10	2011-10-12 08:26:25.398000+02:00	2011-10-12 08:26:25.398000+02:00
+# 2	1158600.0	0.0	1158600.0	1141800.0	0.0	0.0	task-42957	T03 Adjust confirmation of receipt	Group 1	Resource21	2011-11-24 15:36:51.302000+01:00	2011-11-24 15:36:51.302000+01:00
+# 3	1158600.0	0.0	1158600.0	0.0	0.0	0.0	task-47958	T02 Check confirmation of receipt	Group 4	Resource21	2011-11-24 15:37:16.553000+01:00	2011-11-24 15:37:16.553000+01:00
+# 4	0.0	0.0	0.0	0.0	0.0	1.0	task-43021	Confirmation of receipt	EMPTY	Resource30	2011-10-18 13:46:39.679000+02:00	2011-10-18 13:46:39.679000+02:00
+# 5	0.0	0.0	0.0	0.0	0.0	1.0	task-43672	T06 Determine necessity of stop advice	Group 1	Resource30	2011-10-18 13:47:06.950000+02:00	2011-10-18 13:47:06.950000+02:00
+# 6	0.0	0.0	0.0	0.0	0.0	1.0	task-43671	T02 Check confirmation of receipt	Group 4	Resource30	2011-10-18 13:47:26.235000+02:00	2011-10-18 13:47:26.235000+02:00
+# 7	0.0	0.0	0.0	0.0	0.0	1.0	task-43674	T03 Adjust confirmation of receipt	Group 1	Resource30	2011-10-18 13:47:41.811000+02:00	2011-10-18 13:47:41.811000+02:00
+# 8	0.0	0.0	0.0	0.0	0.0	1.0	task-43675	T02 Check confirmation of receipt	Group 4	Resource30	2011-10-18 13:47:57.979000+02:00	2011-10-18 13:47:57.979000+02:00
+# 9	0.0	0.0	0.0	0.0	0.0	1.0	task-43673	T10 Determine necessity to stop indication	Group 1	Resource30	2011-10-18 13:48:15.357000+02:00	2011-10-18 13:48:15.357000+02:00
+# 10	0.0	0.0	0.0	0.0	0.0	1.0	task-43676	T03 Adjust confirmation of receipt	Group 1	Resource30	2011-10-18 13:48:30.632000+02:00	2011-10-18 13:48:30.632000+02:00
+# 
+# =============================================================================
+
+
+
+#endf = pd.DataFrame(columns=['caseid','actid','resid','ts'])#,'dur'])#,'partcycleT','partleadT','wasteT','thisWT','rclt'])
+
+n_cases = 0
+n_events = 0
+
+caseid = []
+actid = []
+actseq = []
+resid = []
+ts = []
+startTime = []
+for case in log:
+    actidx = 0
+    startT = case[0]['time:timestamp'].timestamp()
+    for event in case:
+        #endf = endf.append(dict(event), ignore_index = True) #this takes too long time 
+#        endf = endf.append({
+#                'caseid': n_cases,
+#                'actid':event['concept:name'],
+#                'resid':event['org:resource'],
+#                'ts':event['time:timestamp'].timestamp()
+#                }, ignore_index = True)
+        
+        caseid.append(n_cases)
+        actid.append(event['concept:name'])
+        actseq.append(actidx)
+        resid.append(event['org:resource'])
+        ts.append(event['time:timestamp'].timestamp())
+        startTime.append(event['time:timestamp'].timestamp() - startT)
+        actidx = actidx + 1
+        n_events = n_events + 1
+    n_cases = n_cases + 1
+            
+df = pd.DataFrame({'caseid': caseid, 
+                   'actid':actid, 
+                   'actseq':actseq, 
+                   'resid':resid, 
+                   'ts':ts, 
+                   'sT': startTime})        
+
+df['preid'] = df['actid'].shift(1)
+df['preid'] = df.apply(lambda row: row['preid'] if row['actseq']!=0 else 'START', axis = 1)
+
+df['dur'] = 0
+dur = list(df['dur'])
+
+evS = 0
+evE = -1
+for i in range(0, len(df)):
+    if df['actseq'][i] == 0:
+        evS = i
+    if i < len(df) - 1:
+        if df['actseq'][i + 1] == 0:
+            evE = i
+    else:
+        evE = i
+        
+    if evE >= evS:
+        #df['dur'][evS:evE] = df['sT'][evE]
+        for j in range(evS, evE+1):
+            #print(df['sT'][evE])
+            dur[j] = df['sT'][evE-1]
+df['dur'] = dur
+
+
+sort_df = df.sort_values(by=['dur','caseid', 'actseq'], ascending = [0,1,1])
+sortid = 0
+#sort_df['sid'] = 0
+sid = np.zeros(len(sort_df))
+for i in range(1, len(sort_df)):
+    if i < len(sort_df) - 1:
+        if sort_df.iloc[i,:]['caseid'] != sort_df.iloc[i-1,:]['caseid']:
+            sortid = sortid + 1
+
+    sid[i] = sortid
+    
+sort_df['sid'] = sid
+
+activities = log_attributes_filter.get_attribute_values(log, "concept:name")
+actid = []
+cnt = []
+for act0 in activities.items():
+    actid.append(act0[0])
+    cnt.append(act0[1])
+
+act_df = pd.DataFrame({'id':actid, 'cnt':cnt})
+
+n_activities = len(act_df)
+
+from pm4py.statistics.traces.log import case_statistics
+traces = case_statistics.get_variant_statistics(log)#, parameters={"max_variants_to_return": 5})
+
+#acts = []
+#cnt = []
+#tid = []
+#idx = 0
+#for trace in traces:
+#    tid.append(idx)
+#    acts.append(trace['variant'])
+#    cnt.append(trace['count'])
+#    idx = idx + 1
+#    
+#trace_df = pd.DataFrame({'id': tid, 'acts': acts, 'cnt':cnt})
+#n_traces = len(trace_df)
+
+tid = []
+actid = []
+actseq = []
+cnt = []
+n_traces = 0
+for trace in traces:
+    actidx = 0
+    acts = trace['variant']
+    for s in acts.split(','):
+        tid.append(n_traces)
+        actid.append(s)
+        actseq.append(actidx)
+        cnt.append(trace['count'])
+        actidx = actidx+1
+    n_traces = n_traces + 1
+    
+trace_df = pd.DataFrame({'id': tid, 'actid': actid, 'actseq':actseq, 'cnt':cnt})
+trace_df['preid'] = trace_df['actid'].shift(1)
+trace_df['preid'] = trace_df.apply(lambda row: row['preid'] if row['actseq']!=0 else 'START', axis = 1)
+#trace_df['postid'] = trace_df['actid'].shift(1)
+#trace_df['postid'] = trace_df.apply(lambda row: row['preid'] if row['actseq']!=0 else 'START', axis = 1)
+
+trace_df['pre_post'] = trace_df.apply(lambda row: row['preid']+"-"+row['actid'], axis = 1)
+
+# matrix
+df['pre_post'] = df.apply(lambda row: row['preid']+"-"+row['actid'], axis = 1)
+#mtxdf1 = pd.DataFrame({'ant':df['preid'],'con':df})
+mtxdf1 = df[df['preid']!='START'].groupby('pre_post')['caseid'].count() #agg(['sum','count','mean'])
+#mtxdf1['abs'] = mtxdf1['sum']/mtxdf1['count']
+mtxdf= pd.DataFrame({'pre_post':mtxdf1.index, 'cnt': list(mtxdf1)})
+
+#roles Detection: related to resource vs activity?
+#from pm4py.algo.enhancement.roles import factory as roles_factory
+#roles = roles_factory.apply(log)
+
