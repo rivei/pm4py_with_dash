2cf32ef124f653fdd13872cb315f564f943c2a1e
Wei Lin <weilin0304@gmail.com>
2019-12-03 07:43:51 +0800

    add plot functions

--- python/dash_app.py
+++ python/dash_app.py
@@ -7,20 +7,18 @@ Created on Sun Nov 24 22:49:04 2019
 
 # -*- coding: utf-8 -*-
 #import base64
-#import io
+import io
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objs as go
 import pandas as pd
 import numpy as np
-#import pathlib
+import pathlib
 
 from dash.dependencies import Input, Output, State
 from scipy import stats
-import pm4pyPlus as ppp
-
-#import process_csv as pscsv
+import pm4pyPlus as ppd
+import goPlots as gpp
 
 group_colors = {"control": "light blue", "reference": "red"}
 
@@ -29,64 +27,11 @@ app = dash.Dash(
 )
 server = app.server
 
-#PATH = pathlib.Path(__file__).parent
-#DATA_PATH = PATH.joinpath("data").resolve()
-#default_csv_data = pd.read_csv(DATA_PATH.joinpath("running-example.csv"))
-#default_study_data = pd.read_csv(DATA_PATH.joinpath("study.csv"))
+PATH = pathlib.Path(__file__).parent
+DATA_PATH = PATH.joinpath("data").resolve()
 
-#from pm4py.objects.log.importer.xes import factory as xes_importer
-#default_log = xes_importer.apply(str(DATA_PATH.joinpath("receipt.xes")))
-
-testdf = ppp.trace_df[ppp.trace_df['id']==0]
-#
-#ttfig = go.Figure(data=[
-#    go.Bar(x=testdf['actseq'], y=testdf['id'],orientation='h')
-#])
-## Change the bar mode
-#ttfig.update_layout(barmode='stack')
-#
-import plotly.express as px
-#ttfig = px.bar(ppp.trace_df, x="actseq", y="id", color='actid', orientation='h',
-#             #hover_data=["tip", "size"],
-#             height=1000,
-#             title='Top Trace')
-
-ttfig = go.Figure(
-        data = go.Scatter(y = -ppp.trace_df['actseq'],
-                            x = ppp.trace_df['id'],
-                            mode='markers',
-                            marker = dict(color= ppp.trace_df['nactid'],#np.random.randn(120),#ppp.trace_df['actid'],
-                                          colorscale = 'rainbow',#'Cividis',#'Viridis',
-                                          size = 10,
-                                          symbol='square')))
-#ttfig.update_layout(
-#    autosize=False,
-#    width=400,
-#    height=1000,
-#    margin=go.layout.Margin(
-#        l=50,
-#        r=50,
-#        b=100,
-#        t=100,
-#        pad=4
-#    ),
-#    paper_bgcolor="LightSteelBlue",
-#)
-#tips = px.data.tips()
-#ttfig = px.bar(tips, x="total_bill", y="sex", color='day', orientation='h',
-#             hover_data=["tip", "size"],
-#             height=400,
-#             title='Restaurant bills')
-
-
-#time
-dtfig = px.scatter(ppp.sort_df, x="sT", y="sid", color="actid")
-                 #size='petal_length', hover_data=['petal_width'])
-
-#mtxfig1 = go.Figure(data=go.Heatmap(
-#       z=[[1, 20, 30, 50, 1], [20, 1, 60, 80, 30], [30, 60, 1, -10, 20]],
-#       x=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-#       y=['Morning', 'Afternoon', 'Evening']))
+from pm4py.objects.log.importer.xes import factory as xes_importer
+default_log = xes_importer.apply(str(DATA_PATH.joinpath("receipt.xes")))
 
 
 app.layout = html.Div(
@@ -108,116 +53,124 @@ app.layout = html.Div(
             ],
         ),
                 
-        # settings and key elements
         html.Div(
-            className="row",
+            className="app-body",
             children=[
+                # settings
                 html.Div(
-                    className="user-control two columns",
+                    className="card user-control two columns bg-white",
                     children=[
-                        html.H6("Event Log File"),
-                        dcc.Upload(
-                            id="upload-data",
-                            className="upload",
-                            children=html.Div(
-                                children=[
-                                    html.P("Drag and Drop or "),
-                                    html.A("Select Files"),
-                                ]
-                            ),
-                            accept=".csv,.xes"
-                        ),
+                        html.H6("User control"),
+#                        dcc.Upload(
+#                            id="upload-data",
+#                            className="upload",
+#                            children=html.Div(
+#                                children=[
+#                                    html.P("Drag and Drop or "),
+#                                    html.A("Select Files"),
+#                                ]
+#                            ),
+#                            accept=".csv,.xes"
+#                        ),
                     ],
                 ),
                 html.Div(
-                    id="info-container",
-                    className="row container-display",            
+                    id="main-content-container",
+                    className="card-left nine columns",            
                     children=[
-                        html.Div(
-                            [#html.H6(id="eventText"), 
-                                    html.H6(ppp.n_events),
-                                    html.P("No. of Events")],
-                            id="events",
-                            className="mini_container",
-                        ),
-                        html.Div(
-                            [#html.H6(id="caseText"), 
-                                    html.H6(ppp.n_cases),
-                                    html.P("Cases")],
-                            id="cases",
-                            className="mini_container",
-                        ),
-                        html.Div(
-                            [#html.H6(id="traceText"), 
-                                    html.H6(ppp.n_traces),
-                                    html.P("Traces")],
-                            id="traces",
-                            className="mini_container",
-                        ),
-                        html.Div(
-                            [#html.H6(id="activityText"),
-                                    html.H6(ppp.n_activities),
-                                    html.P("Activities")],
-                            id="acitivities",
-                            className="mini_container",
-                        ),
-                        html.Div(
-                            [#html.H6(id="resourceText"), 
-                                    html.P("Resources")],
-                            id="resources",
-                            className="mini_container",
-                        ),
+                        # statistics
+						html.Div(
+							id="stats-container",
+                            className="container-display body",
+							children=[
+								html.Div(
+									id="events",
+									className="mini_container",
+									children=[
+											html.H6(ppd.n_events(default_log)),
+											html.P("Events")],
+								),
+								html.Div(
+									id="cases",
+									className="mini_container",
+									children=[ 
+											html.H6(ppd.n_cases(default_log)),
+											html.P("Cases")],
+								),
+								html.Div(
+									id="traces",
+									className="mini_container",
+									children=[ 
+											html.H6(ppd.n_traces(default_log)),
+											html.P("Traces")],
+								),
+								html.Div(
+									id="acitivities",
+									className="mini_container",
+									children=[
+											html.H6(ppd.n_activities(default_log)),
+											html.P("Activities")],
+								),
+								html.Div(
+									id="resources",
+									className="mini_container",
+									children=[ 
+                                            html.H6(ppd.n_traces(default_log)),
+											html.P("Resources")],
+								),
+							],
+						),
+						# Body of the App
+						html.Div(
+							id="tabs_container",
+							className="tabs body bg-white",
+							children=[
+								dcc.Tabs(
+									id="content_tabs",
+									value="ttrace",
+									children= [
+										dcc.Tab(
+											label="Map",
+											value="promap",
+											children=[
+												html.H3("Map"),
+											],
+										),
+										dcc.Tab(
+											label="Top-Trace",
+											value="ttrace",
+											children=[
+												html.H3("top trace"),
+				                                dcc.Graph(figure = gpp.top_trace_plot(default_log)),
+											],
+										),
+										dcc.Tab(
+											label="Sequence",
+											value="seq",
+											children=[
+												html.H3("Sequence"),
+											],
+										),
+										dcc.Tab(
+											label="Time",
+											value="tim",
+											children=[
+												html.H3("Time"),
+				                                dcc.Graph(figure=gpp.dotted_chart(default_log)),
+											],
+										),
+										dcc.Tab(
+											label="Resources",
+											value="res",
+											children=[
+												html.H3("Resources"),
+											],
+										),
+									]
+								),
+							],
+						),
                     ],
-                ),
-            ],
-        ),
-        # Body of the App
-        html.Div(
-            id="content_container",
-            className="row tabs",
-            children=[
-                dcc.Tabs(
-                    id="content_tabs",
-                    value="ttrace",
-                    children= [
-                        dcc.Tab(
-                            label="Map",
-                            value="promap",
-                            children=[
-                                html.H3("Map"),
-                            ],
-                        ),
-                        dcc.Tab(
-                            label="Top-Trace",
-                            value="ttrace",
-                            children=[
-                                html.H3("top trace"),
-                                dcc.Graph(figure = ttfig),
-                            ],
-                        ),
-                        dcc.Tab(
-                            label="Sequence",
-                            value="seq",
-                            children=[
-                                html.H3("Sequence"),
-                            ],
-                        ),
-                        dcc.Tab(
-                            label="Time",
-                            value="tim",
-                            children=[
-                                html.H3("Time"),
-                                dcc.Graph(figure=dtfig),
-                            ],
-                        ),
-                        dcc.Tab(
-                            label="Resources",
-                            value="res",
-                            children=[
-                                html.H3("Resources"),
-                            ],
-                        ),
-                    ]
                 ),
             ],
         ),
---
+++ python/goPlots.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  3 07:06:25 2019
+
+@author: Wei
+"""
+
+import plotly.express as px
+import plotly.graph_objs as go
+import pm4pyPlus as ppd
+
+def top_trace_plot(log):
+#ttfig = go.Figure(data=[
+#    go.Bar(x=testdf['actseq'], y=testdf['id'],orientation='h')
+#])
+## Change the bar mode
+#ttfig.update_layout(barmode='stack')
+#
+#ttfig = px.bar(ppd.trace_df, x="actseq", y="id", color='actid', orientation='h',
+#             #hover_data=["tip", "size"],
+#             height=1000,
+#             title='Top Trace')
+    df = ppd.traces_df(log)
+    return go.Figure(
+            data = go.Scatter(y = -df['actseq'],
+                                x = df['id'],
+                                mode='markers',
+                                marker = dict(color= df['nactid'],#np.random.randn(120),#ppd.trace_df['actid'],
+                                              colorscale = 'rainbow',#'Cividis',#'Viridis',
+                                              size = 10,
+                                              symbol='square')))
+
+##ttfig.update_layout(
+##    autosize=False,
+##    width=400,
+##    height=1000,
+##    margin=go.layout.Margin(
+##        l=50,
+##        r=50,
+##        b=100,
+##        t=100,
+##        pad=4
+##    ),
+##    paper_bgcolor="LightSteelBlue",
+##)
+##tips = px.data.tips()
+##ttfig = px.bar(tips, x="total_bill", y="sex", color='day', orientation='h',
+##             hover_data=["tip", "size"],
+##             height=400,
+##             title='Restaurant bills')
+#
+#
+    
+def dotted_chart(log):
+    df = ppd.sort_df(log)
+    return px.scatter(df, x="sT", y="sid", color="actid")
+                     #size='petal_length', hover_data=['petal_width'])
+#
+##mtxfig1 = go.Figure(data=go.Heatmap(
+##       z=[[1, 20, 30, 50, 1], [20, 1, 60, 80, 30], [30, 60, 1, -10, 20]],
+##       x=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+##       y=['Morning', 'Afternoon', 'Evening']))
+#
